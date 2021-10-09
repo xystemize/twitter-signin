5267a62c5bc240ddb35d9a8d81c609a1d0ba49d5
Jesse Katsumata <niconico.clarinet@gmail.com>
2021-10-09 16:56:58 +0900

    Add return types for login (#8)

--- src/TwitterSignIn.ts
+++ src/TwitterSignIn.ts
@@ -3,8 +3,26 @@ import { NativeModules } from "react-native";
 const { RNTwitterSignIn } = NativeModules;
 
 type Auth = {
+  /**
+   * Twitter AuthToken
+   */
   authToken?: string;
+  /**
+   * Twitter AuthToken Secret
+   */
   authTokenSecret?: string;
+  /**
+   * email of logged in user
+   */
+  email?: string;
+  /**
+   * Twitter userID
+   */
+  userID?: string;
+  /**
+   * Twitter userName
+   */
+  userName?: string;
 };
 
 export const init = (
